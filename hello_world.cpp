4073e845b19eb2b5433e3550512ae238a76d94af
Felipe Rezende Gardin <gardinfelipe@gmail.com>
2024-03-27 22:50:38 -0300

    adding first logic

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,5 +3,28 @@
 using namespace std;
 
 int main() {
-    cout << "Hello World";
+    cout << "*************************************" << endl;
+    cout << "* Bem vindos ao jogo da adivinhação *" << endl;
+    cout << "*************************************" << endl;
+
+    int numero_secreto = 42;
+
+    int chute;
+
+    cout << "Qual é o seu chute? ";
+    cin >> chute;
+
+    cout << "O valor do seu chute é: " << chute << endl;
+
+    if (chute == numero_secreto) {
+        cout << "Parabens! Você acertou o número secreto!" << endl;
+    }
+    else if (chute > numero_secreto) {
+        cout << "Seu chute foi maior que o número secreto" << endl;
+    }
+    else {
+        cout << "Seu chute foi menor que o número secreto" << endl;
+    }
+
+    return 0;
 }
